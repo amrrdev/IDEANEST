e0d06ecad81aa1bbc1e6afa6471a06a6aed0d982
amrrdev <amrrdev@gmail.com>
2024-10-24 23:16:51 +0300

    feat: create User schema with name, email, and password fields

---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,14 @@
+import { Prop, Schema } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class User extends Document {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true, minlength: 8 })
+  password: string;
+}
