b24e7b7ddffb3031b228f768feda3bff9e3aa0e9
amrrdev <amrrdev@gmail.com>
2024-10-24 22:41:46 +0300

    Initial commit
    - Set up basic NestJS application
    - Configured environment variables using .env

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
