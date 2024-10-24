c68fd67e5092aae276bca9880c8a604ad3428a8f
amrrdev <amrrdev@gmail.com>
2024-10-24 23:19:29 +0300

    feat: create UserSchema using SchemaFactory for Mongoose

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,4 +1,4 @@
-import { Prop, Schema } from '@nestjs/mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
 @Schema()
@@ -12,3 +12,5 @@ export class User extends Document {
   @Prop({ required: true, minlength: 8 })
   password: string;
 }
+
+export const UserSchema = SchemaFactory.createForClass(User);
