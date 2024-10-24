a1b8b689bca5a21588f1d5b405486ce9d0e179b4
amrrdev <amrrdev@gmail.com>
2024-10-24 23:46:32 +0300

    feat: configure IamModule with authentication services and controllers

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { HashingService } from './hashing/hashing.service';
+import { BcryptService } from './hashing/bcrypt.service';
+import { AuthenticationController } from './authentication/authentication.controller';
+import { AuthenticationService } from './authentication/authentication.service';
 
-@Module({})
+@Module({
+  providers: [
+    {
+      provide: HashingService,
+      useClass: BcryptService,
+    },
+    AuthenticationService,
+  ],
+  controllers: [AuthenticationController],
+})
 export class IamModule {}
