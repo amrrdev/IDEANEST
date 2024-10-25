82b00ea46e2d534784179eb917d392da7383f8aa
amrrdev <amrrdev@gmail.com>
2024-10-25 17:22:02 +0300

    feat: add access_level property to User schema
    - Added ccess_level with enum AccessLevel and default value MEMBER for role-based access control in User schema

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,5 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import mongoose, { Document } from 'mongoose';
+import { AccessLevel } from '../enums/access-level.enum';
+import { Organization } from '../../organizations/schemas/organization.schema';
 
 @Schema()
 export class User extends Document {
@@ -11,6 +13,9 @@ export class User extends Document {
 
   @Prop({ required: true, minlength: 8 })
   password: string;
+
+  @Prop({ required: true, enum: AccessLevel, default: AccessLevel.MEMBER })
+  access_level: AccessLevel;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
