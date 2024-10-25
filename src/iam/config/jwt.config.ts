0341075fc89eca430542ca7f808e9f873047bfe0
amrrdev <amrrdev@gmail.com>
2024-10-25 14:33:20 +0300

    feat(config): Add refresh token TTL to JWT configuration

--- src/iam/config/jwt.config.ts
+++ src/iam/config/jwt.config.ts
@@ -6,5 +6,6 @@ export default registerAs('jwt', () => {
     audience: process.env.JWT_TOKEN_AUDIENCE,
     issuer: process.env.JWT_TOKEN_ISSUER,
     accessTokenTtl: process.env.JWT_ACCESS_TOKEN_TTL,
+    refreshTokenTtl: process.env.JWT_REFRESH_TOKEN_TTL,
   };
 });
