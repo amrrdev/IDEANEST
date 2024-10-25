ec88bd934668377c2d10bcf414deea664e946e0c
amrrdev <amrrdev@gmail.com>
2024-10-25 11:33:57 +0300

    feat: register JwtModule asynchronously with custom config, add AuthenticationService and guards to providers

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -5,17 +5,30 @@ import { AuthenticationController } from './authentication/authentication.contro
 import { AuthenticationService } from './authentication/authentication.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from '../users/schemas/user.schema';
+import { JwtModule } from '@nestjs/jwt';
+import jwtConfig from './config/jwt.config';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { AccessTokenGuard } from './authentication/guards/access-token/access-token.guard';
+import { AuthenticationGuard } from './authentication/guards/authentication.guard';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+    ConfigModule.forFeature(jwtConfig),
   ],
   providers: [
     {
       provide: HashingService,
       useClass: BcryptService,
     },
+    {
+      provide: APP_GUARD,
+      useClass: AuthenticationGuard,
+    },
     AuthenticationService,
+    AccessTokenGuard,
   ],
   controllers: [AuthenticationController],
 })
