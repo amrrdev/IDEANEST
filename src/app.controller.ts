c7bb764ce211aadb964ffd38f98a464df5393f96
amrrdev <amrrdev@gmail.com>
2024-10-25 17:21:18 +0300

    feat: add Auth decorator to AppController
    - Applied Auth decorator with AuthType.NONE to getHello endpoint for flexible authentication handling

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Auth } from './iam/authentication/decorators/auth.decorator';
+import { AuthType } from './iam/authentication/enums/auth-type.enum';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Auth(AuthType.NONE)
   getHello(): string {
     return this.appService.getHello();
   }
